92da710c42022009385822068d30acc20691a0b5
Lê Gia Hoàng <giahoangth@gmail.com>
2018-01-02 16:24:45 +0700

    data binding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [NativeScriptModule],
+  imports: [NativeScriptModule,
+    NativeScriptFormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
